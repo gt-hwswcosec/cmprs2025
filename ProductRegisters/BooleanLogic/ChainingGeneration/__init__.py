df17f1ff88e4838391399d901d37eceab2742650
allahverdia <165601933+allahverdia@users.noreply.github.com>
2025-01-06 17:15:45 -0500

    Update __init__.py

--- ProductRegisters/BooleanLogic/ChainingGeneration/__init__.py
+++ ProductRegisters/BooleanLogic/ChainingGeneration/__init__.py
@@ -1,2 +1,5 @@
+# (c) 2025 Georgia Institute of Technology
+# This code is licensed under the MIT license (see LICENSE for details)
+
 import ProductRegisters.BooleanLogic.ChainingGeneration.TemplateBuilding
 import ProductRegisters.BooleanLogic.ChainingGeneration.Templates
