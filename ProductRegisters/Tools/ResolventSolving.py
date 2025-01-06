916f4edf64b9bbcdbd1d1386e7516c130073fd4f
allahverdia <165601933+allahverdia@users.noreply.github.com>
2025-01-06 16:52:37 -0500

    Update ResolventSolving.py

--- ProductRegisters/Tools/ResolventSolving.py
+++ ProductRegisters/Tools/ResolventSolving.py
@@ -1,3 +1,6 @@
+# (c) 2025 Georgia Institute of Technology
+# This code is licensed under the MIT license (see LICENSE for details)
+
 from ProductRegisters.BooleanLogic import XOR, CONST
 from ProductRegisters.Tools.RegisterSynthesis.lfsrSynthesis import berlekamp_massey, berlekamp_massey_iterator
 
