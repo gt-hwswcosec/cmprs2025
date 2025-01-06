2899daee8dd807db7155247f8b3d06dee077d42f
allahverdia <165601933+allahverdia@users.noreply.github.com>
2025-01-06 17:16:16 -0500

    Update fast_template.py

--- ProductRegisters/BooleanLogic/ChainingGeneration/Templates/fast_template.py
+++ ProductRegisters/BooleanLogic/ChainingGeneration/Templates/fast_template.py
@@ -1,3 +1,6 @@
+# (c) 2025 Georgia Institute of Technology
+# This code is licensed under the MIT license (see LICENSE for details)
+
 import ProductRegisters.BooleanLogic.ChainingGeneration.TemplateBuilding as tb
 from ProductRegisters.BooleanLogic.Gates import *
 from ProductRegisters.BooleanLogic.Inputs import *
