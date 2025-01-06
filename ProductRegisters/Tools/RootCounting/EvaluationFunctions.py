8eaad6e45a4cedd9790ac0b8c3216145d57282f6
allahverdia <165601933+allahverdia@users.noreply.github.com>
2025-01-06 16:54:04 -0500

    Update EvaluationFunctions.py

--- ProductRegisters/Tools/RootCounting/EvaluationFunctions.py
+++ ProductRegisters/Tools/RootCounting/EvaluationFunctions.py
@@ -1,3 +1,6 @@
+# (c) 2025 Georgia Institute of Technology
+# This code is licensed under the MIT license (see LICENSE for details)
+
 from ProductRegisters.Tools.RootCounting.Combinatorics import binsum
 
 def optimistic_evaluation(b,c):
