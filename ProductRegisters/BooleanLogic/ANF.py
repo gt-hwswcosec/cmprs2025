7153acd7940352340315cc7d296aab7aef456a92
allahverdia <165601933+allahverdia@users.noreply.github.com>
2025-01-06 17:15:36 -0500

    Update ANF.py

--- ProductRegisters/BooleanLogic/ANF.py
+++ ProductRegisters/BooleanLogic/ANF.py
@@ -1,3 +1,6 @@
+# (c) 2025 Georgia Institute of Technology
+# This code is licensed under the MIT license (see LICENSE for details)
+
 from ProductRegisters.BooleanLogic.BooleanFunction import BooleanFunction
 from ProductRegisters.BooleanLogic.Gates import XOR, AND
 from ProductRegisters.BooleanLogic.Inputs import CONST, VAR
