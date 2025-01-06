5eafc44c86f8cec0f1207625faa989525d74e9e0
allahverdia <165601933+allahverdia@users.noreply.github.com>
2025-01-06 17:14:55 -0500

    Update __init__.py

--- ProductRegisters/BooleanLogic/__init__.py
+++ ProductRegisters/BooleanLogic/__init__.py
@@ -1,3 +1,5 @@
+# (c) 2025 Georgia Institute of Technology
+# This code is licensed under the MIT license (see LICENSE for details)
 
 from ProductRegisters.BooleanLogic.BooleanFunction import BooleanFunction
 from ProductRegisters.BooleanLogic.Gates import *
