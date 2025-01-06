cbe78d62e28b9523607d9a583fcb138293376daa
allahverdia <165601933+allahverdia@users.noreply.github.com>
2025-01-06 17:16:00 -0500

    Update __init__.py

--- ProductRegisters/BooleanLogic/ChainingGeneration/Templates/__init__.py
+++ ProductRegisters/BooleanLogic/ChainingGeneration/Templates/__init__.py
@@ -1,3 +1,6 @@
+# (c) 2025 Georgia Institute of Technology
+# This code is licensed under the MIT license (see LICENSE for details)
+
 from ProductRegisters.BooleanLogic.ChainingGeneration.Templates.arman_template import arman_template
 from ProductRegisters.BooleanLogic.ChainingGeneration.Templates.three_majority_template import three_majority_template
 from ProductRegisters.BooleanLogic.ChainingGeneration.Templates.old_ANF_template import old_ANF_template
