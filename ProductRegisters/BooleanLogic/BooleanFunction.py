0dd1db2c6e7cfb078cd5fa0723a20f9ddeadd911
allahverdia <165601933+allahverdia@users.noreply.github.com>
2025-01-06 17:15:27 -0500

    Update BooleanFunction.py

--- ProductRegisters/BooleanLogic/BooleanFunction.py
+++ ProductRegisters/BooleanLogic/BooleanFunction.py
@@ -1,3 +1,6 @@
+# (c) 2025 Georgia Institute of Technology
+# This code is licensed under the MIT license (see LICENSE for details)
+
 from numba import njit
 from memoization import cached
 import types
