66adf35e4d66019975566cdd5938dff677923410
allahverdia <165601933+allahverdia@users.noreply.github.com>
2025-01-06 16:54:42 -0500

    Update nlfsrSynthesis.py

--- ProductRegisters/Tools/RegisterSynthesis/nlfsrSynthesis.py
+++ ProductRegisters/Tools/RegisterSynthesis/nlfsrSynthesis.py
@@ -1,3 +1,6 @@
+# (c) 2025 Georgia Institute of Technology
+# This code is licensed under the MIT license (see LICENSE for details)
+
 from ProductRegisters.BooleanLogic import XOR, AND, NOT, CONST, VAR
 
 
